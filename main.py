ac6debce08de91e3e8d3d3bbe274eba74ea3c409
Jan Warchocki <j.warchocki@student.tudelft.nl>
2021-12-02 14:33:55 +0100

    Minor changes to main.py

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from sklearn.metrics import accuracy_score
 # Some notes: add a pipeline, speed up mediapipe
 
 if __name__ == '__main__':
-    loader = DefaultLoader('asl_alphabet_train/asl_alphabet_train/')
+    loader = DefaultLoader('data/asl_alphabet_train/')
     loader.create_digit_landmarks()
 
     # mp = MediaPipe()
     # mp.show_landmarks('asl_alphabet_train/asl_alphabet_train/A/A100.jpg')
 
-    data = pd.read_csv('asl_alphabet_train/asl_alphabet_train/digit_landmarks.csv')
+    data = loader.load_digit_landmarks('data/asl_alphabet_train/digit_landmarks.csv')
     X = data.iloc[:, :63]
     y = data['label']
     print(X.head())
