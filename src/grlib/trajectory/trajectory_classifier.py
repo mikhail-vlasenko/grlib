7e316b3ee81e780ce9af8abda1e0e3e5ecaab3c3
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2022-09-21 00:05:47 +0200

    trajectory classifier avg trajectories as _state

--- src/grlib/trajectory/trajectory_classifier.py
+++ src/grlib/trajectory/trajectory_classifier.py
@@ -7,7 +7,7 @@ from src.grlib.trajectory.general_direction_trajectory import GeneralDirectionTr
 
 class TrajectoryClassifier:
     def __init__(self):
-        self._state: Dict[np.ndarray, List[str]] = dict()
+        self.avg_trajectories: Dict[str, np.ndarray] = dict()
 
     def fit(
             self,
@@ -15,6 +15,7 @@ class TrajectoryClassifier:
             gestures: Union[np.ndarray, List[str]]
     ):
         # todo: average out the input trajectories, so that we only have a single trajectory per class
+        # todo: make use of the GeneralDirectionTrajectory type?
         """
         Remembers correspondence between gestures classes and their trajectories
         :param gestures: list of classification answers that the trajectories correspond to
@@ -29,21 +30,34 @@ class TrajectoryClassifier:
             else:
                 temp_gestures[gestures[i]].append(trajectories[i])
 
-        avg_trajectories: Dict[str, np.ndarray] = dict()
         for key, value in temp_gestures.items():
             value = np.array(value)
-            avg_trajectories[key] = value.mean(axis=0)
+            # averaged trajectories are rounded to valid trajectories (-1, 0, 1 terms)
+            self.avg_trajectories[key] = np.array(np.around(value.mean(axis=0)), dtype=int)
 
-        for key, value in avg_trajectories.items():
-            if value not in self._state:
-                self._state[value] = [key]
-            else:
-                self._state[value].append(key)
-
-    def predict(self, trajectory) -> List[str]:
+    def predict(self, given_trajectory) -> List[str]:
         """
         Lists the gestures that are known to follow the given trajectory
-        :param trajectory: the trajectory
+        :param given_trajectory: the trajectory
         :return: list of gesture classes
         """
-        return self._state[trajectory]
+        # TODO: O(n * traj_len) predict is not quick
+        ans = []
+        for pred_class, trajectory in self.avg_trajectories.items():
+            if self.eq_trajectories(given_trajectory, trajectory):
+                ans.append(pred_class)
+        return ans
+
+    @staticmethod
+    def eq_trajectories(traj1, traj2) -> bool:
+        """
+        Determines if the trajectories are close enough to be considered equal
+        :param traj1:
+        :param traj2:
+        :return:
+        """
+        assert len(traj1) == len(traj2)
+        for i in range(len(traj1)):
+            if traj1[i] != traj2[i]:
+                return False
+        return True
