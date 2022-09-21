968dde3ea768bc7bf7cfb86800fc0660ce20cbe0
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2022-09-22 01:38:23 +0200

    trajectory candidates. Simple dynamic gestures work

--- src/grlib/dynamic_detection.py
+++ src/grlib/dynamic_detection.py
@@ -1,9 +1,16 @@
+from typing import List
+
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 
+from src.grlib.feature_extraction.mediapipe_landmarks import MediaPipe
+from src.grlib.feature_extraction.pipeline import Pipeline
+from src.grlib.trajectory.trajectory_candidate import TrajectoryCandidate
+from src.grlib.trajectory.trajectory_classifier import TrajectoryClassifier
+
 
 class DynamicDetector:
-    def __init__(self, start_shapes, y, pipeline, start_pos_confidence):
+    def __init__(self, start_shapes, y, pipeline, start_pos_confidence, trajectory_classifier):
         """
 
         :param start_shapes:
@@ -14,23 +21,60 @@ class DynamicDetector:
         self.start_detection_model = LogisticRegression()
         self.start_detection_model.fit(np.array(start_shapes), y)
         self.sorted_labels = sorted(y.tolist())
-        self.pipeline = pipeline
+        self.pipeline: Pipeline = pipeline
         self.start_pos_confidence = start_pos_confidence
+        self.trajectory_classifier: TrajectoryClassifier = trajectory_classifier
+
+        self.current_candidates: List[TrajectoryCandidate] = []
+        self.frame_cnt = 0
+        self.update_candidates_every = 10
+        self.last_pred = ""
 
     def analyze_frame(self, frame):
         landmarks = self.pipeline.get_world_landmarks_from_image(frame).flatten().tolist()
         self.pipeline.optimize()
 
+        # WARNING: this is for a single hand
+        hand_position = MediaPipe.hands_spacial_position(
+            self.pipeline.get_landmarks_from_image(frame)
+        )[0]
+        print(hand_position)
+
         # proba because there is no need to be sure that it is a particular class:
         #   if there is a good chance it is, trajectory will determine it
         prediction = self.start_detection_model.predict_proba(np.array([landmarks]))[0]
 
-        possible_classes = []
+        possible_classes: List[str] = []
         for i in range(len(prediction)):
             if prediction[i] >= self.start_pos_confidence:
                 possible_classes.append(self.start_detection_model.classes_[i])
 
+        for p in possible_classes:
+            target_traj = self.trajectory_classifier.avg_trajectories[p]
+            # todo: dont add if already exists a recent record
+            self.current_candidates.append(TrajectoryCandidate(
+                target_traj, p, hand_position, 0.1, self.frame_cnt
+            ))
+
+        self.frame_cnt += 1
+        if self.frame_cnt % self.update_candidates_every:
+            pred = self.update_candidates(hand_position)
+            if pred != "":
+                self.last_pred = pred
+
         return possible_classes
 
-        # todo: take the trajectories of possible classes
-        # dynamically detect trajectory and try to map onto a possible class
+    def update_candidates(self, hand_position) -> str:
+        i = 0
+        while i < len(self.current_candidates):
+            candidate = self.current_candidates[i]
+            if candidate.timestamp < self.frame_cnt - self.update_candidates_every:
+                if not candidate.update(hand_position):
+                    self.current_candidates.pop(i)
+                    i -= 1
+                if candidate.valid:
+                    # clean the current candidates cause we found the gesture
+                    self.current_candidates = []
+                    return candidate.pred_class
+            i += 1
+        return ""
---
+++ src/grlib/trajectory/trajectory_candidate.py
@@ -0,0 +1,52 @@
+import numpy as np
+from numpy import copy
+
+from src.grlib.trajectory.general_direction_builder import Direction
+
+
+class TrajectoryCandidate:
+    def __init__(self, target, prediction_class, init_pos, zero_precision, start_timestamp: float):
+        """
+
+        :param target: what the trajectory should look like
+        :param prediction_class: corresponding class
+        :param init_pos:
+        :param zero_precision: how much is considered enough movement
+            (should correspond to GeneralDirectionBuilder.zero_precision)
+        :param start_timestamp: helps to determine when the candidate is too old
+        """
+        self.target = target
+        self.pred_class = prediction_class
+        self.position = init_pos
+        self.zero_precision = zero_precision
+        self.timestamp = start_timestamp
+
+        self.axi = 2
+
+        self.valid = False
+        # todo: allow more movement along the same axis? (in 2 consecutive calls of update),
+        #   (only if its correct direction)
+
+    def update(self, position) -> bool:
+        """
+        Should be called every X frame (5, 10 or something)
+        :param position: new hand position
+        :return: if the trajectory may still be valid (but not necessarily IS valid)
+        """
+        for i in range(self.axi):
+            lower_boundary = self.position[i] - self.zero_precision
+            upper_boundary = self.position[i] + self.zero_precision
+            direction = Direction.STATIONARY.value
+            if lower_boundary > position[i]:
+                direction = Direction.DOWN.value
+            elif upper_boundary < position[i]:
+                direction = Direction.UP.value
+
+            if direction != self.target[i]:
+                return False
+
+        if len(self.target) == 3:
+            self.valid = True
+        self.target = self.target[3:]
+        self.position = np.copy(position)
+        return True
