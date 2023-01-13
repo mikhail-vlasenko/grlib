sort picture files numerically fix last frame in key_frames remove class GeneralDirectionTrajectory as unnecessary

src/grlib/load_data/dynamic_gesture_loader.py
@@ -3,6 +3,7 @@ from typing import List
 
 import numpy as np
 import pandas as pd
+from natsort import natsorted
 
 from ..feature_extraction.pipeline import Pipeline
 from ..load_data.base_loader import BaseLoader
@@ -77,7 +78,8 @@ class DynamicGestureLoader(BaseLoader):
             files: List[str] = [file for file in os.listdir(curr_path)]
 
             # sorting file names alphabetically will "group" them by prefix
-            files.sort()
+            # in this sort, 2 goes before 10
+            files = natsorted(files)
 
             i = 0
             # iter over dynamic gesture instances
@@ -104,6 +106,7 @@ class DynamicGestureLoader(BaseLoader):
                         handedness = captured_handedness
                     # append only if recognized
                     if len(image_landmarks) > 0:
+                        print(i, curr_path + files[i])
                         gesture_image_landmarks.append(np.array(image_landmarks))
                         gesture_world_landmarks.append(np.array(world_landmarks))
                     i += 1
@@ -125,7 +128,7 @@ class DynamicGestureLoader(BaseLoader):
                     hand_shape_encoding = np.concatenate((hand_shape_encoding, lm), axis=None)
 
                 landmarks_results.append(hand_shape_encoding)
-                trajectory_results.append(trajectory.to_np())
+                trajectory_results.append(trajectory.flatten())
                 handedness_results.append(handedness)
                 # append folder name as class label
                 class_labels.append(folder)

src/grlib/trajectory/general_direction_builder.py
@@ -4,7 +4,6 @@ from typing import List, Union
 import numpy as np
 
 from src.grlib.feature_extraction.mediapipe_landmarks import MediaPipe
-from src.grlib.trajectory.general_direction_trajectory import GeneralDirectionTrajectory
 
 
 class Direction(Enum):
@@ -27,40 +26,35 @@ class GeneralDirectionBuilder:
 
         :param zero_precision: how much is considered "no movement on the axis"
         """
-        self.dimensions = 3
+        self.dimensions = 3  # x, y, and z (always 3 for consistency)
         self.zero_precision = zero_precision
 
     def make_trajectory(
             self,
             landmark_sequence: Union[np.ndarray, List[np.ndarray]],
             hand_num: int = 0
-    ) -> GeneralDirectionTrajectory:
+    ) -> np.ndarray:
         """
         Creates the trajectory
         :param landmark_sequence: a sequence of landmarks from (sample) frames
         :param hand_num: index of hand to make the trajectory for
-        :return: the trajectory encoding as an array
+        :return: the trajectory encoding as a 2d numpy array.
+        3 columns (for x, y, z) and len(landmark_sequence) rows
         """
-        trajectory = [[], [], []]
+        trajectory = []
         last = MediaPipe.hands_spacial_position(landmark_sequence[0])[hand_num]
         for landmark in landmark_sequence[1:]:
+            directions = []
+            current = MediaPipe.hands_spacial_position(landmark)[hand_num]
             for i in range(self.dimensions):
-                current = MediaPipe.hands_spacial_position(landmark)[hand_num]
                 lower_boundary = last[i] - self.zero_precision
                 upper_boundary = last[i] + self.zero_precision
                 if lower_boundary > current[i]:
-                    trajectory[i].append(Direction.DOWN.value)
+                    directions.append(Direction.DOWN.value)
                 elif upper_boundary < current[i]:
-                    trajectory[i].append(Direction.UP.value)
+                    directions.append(Direction.UP.value)
                 else:
-                    trajectory[i].append(Direction.STATIONARY.value)
-        return GeneralDirectionBuilder.object_from_lists(trajectory)
-
-    @staticmethod
-    def object_from_lists(trajectory_list: List[List[Direction]]) -> GeneralDirectionTrajectory:
-        return GeneralDirectionTrajectory(
-            len(trajectory_list[0]),
-            np.array(trajectory_list[0]),
-            np.array(trajectory_list[1]),
-            np.array(trajectory_list[2])
-        )
+                    directions.append(Direction.STATIONARY.value)
+            trajectory.append(directions)
+            last = current
+        return np.array(trajectory)

src/grlib/trajectory/trajectory_candidate.py
@@ -8,7 +8,7 @@ class TrajectoryCandidate:
     Tracks a path of the hand for a specific dynamic gesture.
     If the path taken corresponds to the `target` path, the dynamic gesture is considered `valid`.
     """
-    def __init__(self, target, prediction_class, init_pos, zero_precision, start_timestamp: float):
+    def __init__(self, target: np.ndarray, prediction_class, init_pos, zero_precision, start_timestamp: float):
         """
 
         :param target: what the trajectory should look like
@@ -24,7 +24,7 @@ class TrajectoryCandidate:
         self.zero_precision = zero_precision
         self.timestamp = start_timestamp
 
-        self.axi = 2
+        self.used_axi = {"x": True, "y": True, "z": False}
 
         self.valid = False
         # todo: allow more movement along the same axis? (in 2 consecutive calls of update),
@@ -39,17 +39,19 @@ class TrajectoryCandidate:
         :param position: new hand position
         :return: if the trajectory may still be valid (but not necessarily IS valid)
         """
-        for i in range(self.axi):
-            lower_boundary = self.position[i] - self.zero_precision
-            upper_boundary = self.position[i] + self.zero_precision
-            direction = Direction.STATIONARY.value
-            if lower_boundary > position[i]:
-                direction = Direction.DOWN.value
-            elif upper_boundary < position[i]:
-                direction = Direction.UP.value
+        for i, a in enumerate(["x", "y", "z"]):
+            # only update if the axis is used
+            if self.used_axi[a]:
+                lower_boundary = self.position[i] - self.zero_precision
+                upper_boundary = self.position[i] + self.zero_precision
+                direction = Direction.STATIONARY.value
+                if lower_boundary > position[i]:
+                    direction = Direction.DOWN.value
+                elif upper_boundary < position[i]:
+                    direction = Direction.UP.value
 
-            if direction != self.target[i]:
-                return False
+                if direction != self.target[i]:
+                    return False
 
         if len(self.target) == 3:
             self.valid = True