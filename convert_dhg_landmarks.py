74347259a666a3deef180a33f8c6026c2d577490
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2023-06-15 01:10:21 +0200

    dhg data exploration and preprocessing

---
+++ convert_dhg_landmarks.py
@@ -0,0 +1,55 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+prefix = 'data/DHG2016/gesture_5/finger_2/subject_3/essai_1/'
+frame_n = 50
+file = prefix + f'depth_{frame_n}.png'
+
+
+def remove_palm_center(landmarks):
+    landmarks = landmarks.reshape((landmarks.shape[0], 22, 3))
+    landmarks = np.concatenate((landmarks[:, 0:1, :], landmarks[:, 2:, :]), axis=1)
+    return landmarks.reshape((landmarks.shape[0], 21, 3))
+
+
+def show_image():
+    depth_map = plt.imread(file)
+
+    landmarks = np.loadtxt(prefix + 'skeleton_world.txt')
+    landmarks = landmarks.reshape((landmarks.shape[0], 22, 3))
+
+    landmarks = remove_palm_center(landmarks)
+
+    img_landmarks = np.loadtxt(prefix + 'skeleton_image.txt')
+    img_landmarks = img_landmarks.reshape((landmarks.shape[0], 22, 2))
+
+    plt.imshow(depth_map, cmap='gray')
+
+    for i in range(len(landmarks[frame_n])):
+        l = landmarks[frame_n][i]
+        plt.text(l[0] * 800, -l[1] * 800, i, ha="center", va="center", color="r", fontsize=8)
+        img_l = img_landmarks[frame_n][i]
+        # plt.text(img_l[0], img_l[1], i, ha="center", va="center", color="g", fontsize=8)
+    plt.show()
+
+
+def show_3d():
+    import matplotlib
+    matplotlib.use('TkAgg')
+
+    plt.ion()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    landmarks = np.loadtxt(prefix + 'skeleton_world.txt')
+    landmarks = landmarks.reshape((landmarks.shape[0], 22, 3))
+    landmarks = remove_palm_center(landmarks)
+    for i in range(len(landmarks[frame_n])):
+        ax.scatter(landmarks[frame_n][i][0], landmarks[frame_n][i][1], landmarks[frame_n][i][2], c='r')
+    plt.show(block=True)
+
+
+if __name__ == '__main__':
+    # show_image()
+    show_3d()
---
+++ plot_dhg_trajectories.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')  # use TkAgg backend which shows plots in a new window
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+prefix = 'data/DHG2016/gesture_5/finger_2/subject_3/essai_1/'
+# Input data, each row corresponds to a time point
+data = np.loadtxt(f'{prefix}skeleton_world.txt')
+
+# Convert data into numpy array for easier manipulation
+data_np = np.array(data)
+
+# Calculate the average position at each time point
+averages = np.mean(data_np.reshape((data_np.shape[0], -1, 3)), axis=1)
+
+# Split averages into x, y, and z for plotting
+x_avg, y_avg, z_avg = averages.T
+
+# Create a 3D plot
+plt.ion()
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot average positions over time
+sc = ax.scatter(x_avg, y_avg, z_avg, c=np.arange(len(x_avg)), cmap='viridis')
+
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+plt.title('Average 3D position over time')
+
+# Create an interactive colorbar
+cbar = plt.colorbar(sc)
+cbar.set_label('Time')
+
+# Show the interactive window
+plt.show(block=True)
