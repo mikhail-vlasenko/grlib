5713d3293ef94dee693cbf41cf695325065a6f1b
Mikhail Vlasenko <mikhail.v.vlasenko@gmail.com>
2023-07-01 17:54:43 +0200

    plotting trajectories of our dynamic dataset

--- plot_dhg_trajectories.py
+++ plot_dhg_trajectories.py
@@ -1,15 +1,46 @@
+import os
+
 import numpy as np
 import matplotlib
+from natsort import natsorted
+
+
+from src.grlib.exceptions import NoHandDetectedException
+from src.grlib.feature_extraction.pipeline import Pipeline
+
 matplotlib.use('TkAgg')  # use TkAgg backend which shows plots in a new window
 import matplotlib.pyplot as plt
 
 
-prefix = 'data/DHG2016/gesture_7/finger_2/subject_3/essai_1/'
-# Input data, each row corresponds to a time point
-data = np.loadtxt(f'{prefix}skeleton_world.txt')
+curr_path = 'data\\dynamic_dataset\\fist-up\\'
+splits = [22, 23, 25]
+files = [curr_path + file for file in os.listdir(curr_path)]
+files = natsorted(files)
 
-# Convert data into numpy array for easier manipulation
-data_np = np.array(data)
+pipeline = Pipeline(num_hands=1)
+pipeline.add_stage(0, 0)
+
+landmarks = []
+results = []
+for file_idx, file in enumerate(files):
+    print(file)
+    try:
+        results.append(pipeline.get_landmarks_from_path(file))
+    except NoHandDetectedException:
+        pass
+
+# Remove the instances where no hand was detected (have empty list for landmarks)
+results = [result for result in results if len(result[0]) > 0]
+
+for result in results:
+    landmarks.append(result[0])
+
+data_np = np.array(landmarks)[0 : 22 + 1]
+# data_np = np.array(landmarks)[22 : 22 + 23 + 1]
+
+# DHG
+# prefix = 'data/DHG2016/gesture_1/finger_2/subject_3/essai_1/'
+# data_np = np.loadtxt(f'{prefix}skeleton_world.txt')
 
 # Calculate the average position at each time point
 averages = np.mean(data_np.reshape((data_np.shape[0], -1, 3)), axis=1)
